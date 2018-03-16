da7ed3b1eab5eaca191326d41de82621c6fd5bac
gumbald <gumbald@gmail.com>
2018-03-16 20:21:08 +0000

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'XXXXXXXXXX'
-consumer_secret = 'XXXXXXXXXX'
-access_token = 'XXXXXXXXXX'
-access_token_secret = 'XXXXXXXXXX'
+consumer_key = 'l14dGC7OCwX6D6a05HiD3Rbe9'
+consumer_secret = 'JrzIULlBvgTjo4orlOdl9e1LABueQulVYXNJd2Niz3LnNAp7wI'
+access_token = '323781064699904197-PVD5f52QkrXxN27OrJum0Tz3oYecDTp'
+access_token_secret = 'nbAhH1wuYD8g7VRZG50ZvXcQvvBeQvegLNnvcKL9uy3hm'
